1dbde3318e57b15506866b473a64a42b98763108
MaruT22 <matthewteng322@gmail.com>
2024-05-06 13:04:25 -0400

    Semi complete

--- Question2.py
+++ Question2.py
@@ -4,4 +4,13 @@
 # Author:     Last Name. First Initial
 # Created:    dd/mm/yyyy
 #-------------------------------------------------------------------------
-
+number = 5
+sum = 0
+while number != 20:
+    
+    if number % 3 == 0: 
+        sum = sum + number
+    else:
+        continue
+    number = number + 1
+    print(sum)
